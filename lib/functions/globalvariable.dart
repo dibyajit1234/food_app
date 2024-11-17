158658efdfe372db59610ce984614e5e608ef14a
dibyajit <dibyajitnandi7205@gmail.com>
2024-11-17 11:51:57 +0530

    .

--- lib/functions/globalvariable.dart
+++ lib/functions/globalvariable.dart
@@ -1,185 +1,3 @@
-// final resturants = [
-//   {
-//     'canteen': 'Bengali Canteen',
-//     'image': 'assets/images/bengali-canteen.png',
-//     'products': [
-//       {
-//         'id': 0,
-//         'title': 'Fish Rice',
-//         'price': 80,
-//         'imageUrl': "assets/images/fish curry.png",
-//       },
-//       {
-//         'id': 1,
-//         'title': 'Chicken Rice',
-//         'price': 90,
-//         'imageUrl': "assets/images/chickenrice.png",
-//       },
-//       {
-//         'id': 2,
-//         'title': 'Eggroll',
-//         'price': 40,
-//         'imageUrl': "assets/images/roll.png",
-//       },
-//       {
-//         'id': 3,
-//         'title': 'Bread-omlette',
-//         'price': 35,
-//         'imageUrl': "assets/images/omelette_PNG26.png",
-//       },
-//       {
-//         'id': 4,
-//         'title': 'Chai',
-//         'price': 10,
-//         'imageUrl': "assets/images/Tea-Cup-PNG-Photo-Image.png",
-//       },
-//       {
-//         'id': 5,
-//         'title': 'Mountain Dew',
-//         'price': 20,
-//         'imageUrl': "assets/images/mountaindew.png",
-//       },
-//       {
-//         'id': 6,
-//         'title': 'Maggi',
-//         'price': 30,
-//         'imageUrl': "assets/images/maggie.png",
-//       },
-//       {
-//         'id': 7,
-//         'title': 'Chowmin',
-//         'price': 60,
-//         'imageUrl': "assets/images/chowmin.png",
-//       },
-//       {
-//         'id': 8,
-//         'title': 'paratha',
-//         'price': 60,
-//         'imageUrl': "assets/images/paratha.png",
-//       },
-//     ]
-//   },
-//   {
-//     'canteen': 'Night Canteen',
-//     'image': 'assets/images/night-canteen.jpg',
-//     'products': [
-//       {
-//         'id': 0,
-//         'title': 'Kurkure',
-//         'price': 10,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 1,
-//         'title': 'Coffee',
-//         'price': 20,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 2,
-//         'title': 'eggroll',
-//         'price': 50,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 3,
-//         'title': 'KitKat',
-//         'price': 30,
-//         'imageUrl': "assets/images/fish.png",
-//       }
-//     ]
-//   },
-//   {
-//     'canteen': 'Chinese canteen',
-//     'image': 'assets/images/bengali-canteen.png',
-//     'products': [
-//       {
-//         'id': 0,
-//         'title': 'Tripple rice',
-//         'price': 70,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 1,
-//         'title': 'Mixed Chowmin',
-//         'price': 60,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 2,
-//         'title': 'Chicken Lolipop',
-//         'price': 45,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 3,
-//         'title': 'Tea',
-//         'price': 10,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 4,
-//         'title': 'Aloo Paratha',
-//         'price': 40,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 5,
-//         'title': 'String',
-//         'price': 20,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 6,
-//         'title': 'Chicken Manchurian',
-//         'price': 70,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 7,
-//         'title': 'Butter Chicken',
-//         'price': 310,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//       {
-//         'id': 8,
-//         'title': 'Jera Rice',
-//         'price': 80,
-//         'imageUrl': "assets/images/profile.jpg",
-//       },
-//     ]
-//   },
-//   {
-//     'canteen': 'Veg Canteen',
-//     'image': 'assets/images/bengali-canteen.png',
-//     'products': [
-//       {
-//         'id': 0,
-//         'title': 'Chai',
-//         'price': 10,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 1,
-//         'title': 'Pattis',
-//         'price': 30,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 2,
-//         'title': 'Good Day',
-//         'price': 10,
-//         'imageUrl': "assets/images/fish.png",
-//       },
-//       {
-//         'id': 3,
-//         'title': 'Omlet',
-//         'price': 30,
-//         'imageUrl': "assets/images/fish.png",
-//       }
-//     ]
-//   },
-// ];
 final resturants = [
   {
     'canteen': 'Gurukul Canteen',
@@ -255,43 +73,37 @@ final resturants = [
         'id': 11,
         'title': 'Dairy Milk',
         'price': 20,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/diarymilk.jpeg",
+        'imageUrl': "assets/images/diarymilk.jpeg",
       },
       {
         'id': 12,
         'title': 'KurKure',
         'price': 10,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/kurkure.jpeg",
+        'imageUrl': "assets/images/kurkure.jpeg",
       },
       {
         'id': 13,
         'title': 'Lays',
         'price': 20,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/lays.jpeg",
+        'imageUrl': "assets/images/lays.jpeg",
       },
       {
         'id': 14,
         'title': 'Loti Choco Pie',
         'price': 10,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/lotichocopie.jpeg",
+        'imageUrl': "assets/images/lotichocopie.jpeg",
       },
       {
         'id': 15,
         'title': 'Oreo',
         'price': 10,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/oreo.jpeg",
+        'imageUrl': "assets/images/oreo.jpeg",
       },
       {
         'id': 16,
         'title': 'Sting',
         'price': 20,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/string.jpeg",
+        'imageUrl': "assets/images/string.jpeg",
       },
       {
         'id': 17,
@@ -435,55 +247,55 @@ final resturants = [
         'id': 0,
         'title': 'Tripple rice',
         'price': 70,
-        'imageUrl': "assets/images/fish.png",
+        'imageUrl': "assets/images/triple rice.jpeg",
       },
       {
         'id': 1,
         'title': 'Mixed Chowmin',
         'price': 60,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/chowmin.png",
       },
       {
         'id': 2,
         'title': 'Chicken Lolipop',
         'price': 45,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/chicken lolipop.jpeg",
       },
       {
         'id': 3,
         'title': 'Tea',
         'price': 10,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/tea.jpeg",
       },
       {
         'id': 4,
         'title': 'Aloo Paratha',
         'price': 40,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/aloo paratha.jpeg",
       },
       {
         'id': 5,
         'title': 'Sting',
         'price': 20,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/string.jpeg",
       },
       {
         'id': 6,
         'title': 'Chicken Manchurian',
         'price': 70,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/manchurian.jpeg",
       },
       {
         'id': 7,
         'title': 'Butter Chicken',
         'price': 310,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/butter chicken.jpeg",
       },
       {
         'id': 8,
         'title': 'Jera Rice',
         'price': 80,
-        'imageUrl': "assets/images/profile.jpg",
+        'imageUrl': "assets/images/jirarice.jpeg",
       },
       {
         'id': 9,
@@ -561,25 +373,25 @@ final resturants = [
         'id': 0,
         'title': 'Chai',
         'price': 10,
-        'imageUrl': "assets/images/fish.png",
+        'imageUrl': "assets/images/coffee.jpeg",
       },
       {
         'id': 1,
         'title': 'Pattis',
         'price': 30,
-        'imageUrl': "assets/images/fish.png",
+        'imageUrl': "assets/images/patties.jpeg",
       },
       {
         'id': 2,
         'title': 'Good Day',
         'price': 10,
-        'imageUrl': "assets/images/fish.png",
+        'imageUrl': "assets/images/goodday.jpeg",
       },
       {
         'id': 3,
-        'title': 'Omlet',
+        'title': 'Omlette',
         'price': 30,
-        'imageUrl': "assets/images/fish.png",
+        'imageUrl': "assets/images/omelette_PNG26.png",
       },
       {
         'id': 4,
@@ -591,8 +403,7 @@ final resturants = [
         'id': 5,
         'title': 'Bhujia',
         'price': 10,
-        'imageUrl':
-            "assets/images/WhatsApp Unknown 2024-11-16 at 21.40.27/bhujia.jpeg"
+        'imageUrl': "assets/images/bhujia.jpeg"
       },
       {
         'id': 6,
